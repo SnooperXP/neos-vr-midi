423311c9d0d0355f2ac67e73ddf32cca32ece61d
Joseph Hitzke <joseph.hitzke@gmail.com>
2020-12-23 23:43:57 -0800

    Feat: Selection of midi device, indefinite loop printing input received

---
+++ __init__.py
@@ -0,0 +1,49 @@
+from pygame import midi
+import pprint
+
+midi.init()
+
+supported_interfaces = ['MMSystem', 'ALSA', 'CoreMIDI']
+pp = pprint.PrettyPrinter(indent=2)
+print("Choose midi input device")
+device_count = midi.get_count()
+supported_device_ids = []
+
+for n in range(device_count):
+	current_info = midi.get_device_info(n)
+	current_interface = current_info[0].decode('ascii')
+
+	if current_interface not in supported_interfaces:
+		continue
+
+	input_supported = (current_info[2] == 1)
+
+	if not input_supported:
+		continue
+
+	supported_device_ids.append(str(n))
+
+	device_name = current_info[1].decode('ascii')
+	device_opened = current_info[4]
+
+	print(f"[{n}] {device_name} :: Opened: {device_opened}")
+
+if len(supported_device_ids) == 0:
+	print("No supported devices. Please ensure the devices aren't currenlty in use by another program.")
+	exit()
+
+device_id = input(f"Input device number: ")
+
+while True:
+	if device_id in supported_device_ids:
+		break
+	else:
+		device_id = input(f"Invalid number, please input valid device number: ")
+
+device = midi.Input(int(device_id))
+print("Device connected!")
+
+while True:
+	if device.poll():
+		event = device.read(1)[0]
+		print(f"Clock: {event[1]} :: Status: {event[0][0]} :: Data1: {event[0][1]} :: Data2: {event[0][2]} :: Data3: {event[0][3]}")
